275f993d0d58cc08276584cd96f823756c87b5c6
Kemosabe2911 <stevinprince29@gmail.com>
2021-02-05 10:04:11 +0530

    Connected MongoDB

---
+++  config/key.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MongoURI: 'mongodb+srv://Kemosabe2911:Stevin@123@tasks-cwohk.mongodb.net/rever?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const express= require('express')
-
+const express= require('express');
 const app= express();
+const mongoose = require('mongoose');
+
+//Config MongoDB
+const db= require('./ config/key').MongoURI;
+
+//Connect to MongoDB
+mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{console.log("Connected to MongoDB")})
+.catch((err)=> {console.log(err)});
+
 
 const PORT = process.env.PORT || 5000;
 
